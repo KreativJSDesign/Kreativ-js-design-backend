f86ed151806a7fde316070c964fb95c3537eabe9
Tushar Raj <tushar.raj@ensuesoft.com>
2025-03-26 20:36:13 +0530

    Minor change

--- src/controllers/etsy.controller.ts
+++ src/controllers/etsy.controller.ts
@@ -73,7 +73,7 @@ export const CreateOrderWebhook = async (req: any, res: any): Promise<void> => {
 
     const webhookPayload = {
       event_name: "listings.update",
-      callback_url: "https://kreativjsdesignapi.vercel.app/api/etsy/listings",
+      callback_url: "https://kreativjsdesign.de/api/etsy/listings",
       shop_id: shopId,
     };
 
